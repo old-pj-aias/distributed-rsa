872734bdf8806cb0ef0613c1d52e5a2dee310900
akakou <akakou571@gmail.com>
2020-08-30 00:14:00 +0900

    [add] Calc a - b % n

--- src/utils.rs
+++ src/utils.rs
@@ -7,6 +7,19 @@ pub fn generate_random_ubigint(size: usize) -> BigUint {
     return BigUint::new(random_bytes);
 }
 
+pub fn submod(a: BigUint, b: BigUint, n: BigUint) -> BigUint {
+    let a  = a % n.clone();
+    let b  = b % n.clone();
+
+    if a > b {
+        a - b
+    }
+    else {
+        n - (b - a)
+    }
+
+}
+
 #[test]
 fn test_generate_random_ubigint() {
     for i in 1..20 {
@@ -14,4 +27,34 @@ fn test_generate_random_ubigint() {
         let random = generate_random_ubigint(size);
         println!("{:x}\n\n\n", random);        
     }
+}
+
+#[test]
+fn test_submod1() {
+    let n = BigUint::from(10 as u16);
+
+    let a = BigUint::from(5 as u16);
+    let b = BigUint::from(8 as u16);
+
+    let expected = BigUint::from(7 as u16);
+    let result = submod(a, b, n);
+
+    println!("{},{}", result, expected);
+
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn test_submod2() {
+    let n = BigUint::from(10 as u16);
+
+    let a = BigUint::from(5 as u16);
+    let b = BigUint::from(8 as u16);
+
+    let expected = BigUint::from(3 as u16);
+    let result = submod(b, a, n);
+
+    println!("{},{}", result, expected);
+
+    assert_eq!(result, expected);
 }
