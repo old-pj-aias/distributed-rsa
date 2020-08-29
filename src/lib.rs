aff0b8bdea37e86e0f9fb88cd1f8c0a51521b745
akakou <akakou571@gmail.com>
2020-08-29 23:28:12 +0900

    [add] Generate distributed RSA Private keys

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
+mod utils;
+
+use crate::utils::generate_random_ubigint;
+
 extern crate rsa;
 extern crate rand;
 
 extern crate num_bigint_dig as num_bigint;
 extern crate num_traits;
+
+use std::vec::Vec;
 
 use rsa::{BigUint, RSAPrivateKey};
 use rand::rngs::OsRng;
@@ -18,14 +24,31 @@ pub struct DistributedRSAPrivateKeySet {
 }
 
 impl DistributedRSAPrivateKeySet {
-    pub fn from_rsa_private_key (private_key: &RSAPrivateKey) {
-        private_key.d();
+    pub fn from_rsa_private_key (private_key: &RSAPrivateKey, partitions: u32) -> Self {
+        let mut private_keys = Vec::new();
+
+        let d = private_key.d();
         let primes = private_key.primes();
 
-        let one = BigUint::from(1 as u32);
+        let one = BigUint::from(1 as u16);
         let lambda = (&primes[0].clone() - &one) * (&primes[1] - &one);
 
-        println!("{}", lambda);
+        let mut remain = d.clone();
+
+        for _ in 0..partitions -1 {
+            let random = generate_random_ubigint(512) % lambda.clone();
+            let key = DistributedRSAPrivateKey{ d: random.clone() % lambda.clone() };
+            private_keys.push(key);
+
+            remain = remain - random;
+        }
+
+        let key = DistributedRSAPrivateKey{ d: remain };
+        private_keys.push(key);
+
+        return DistributedRSAPrivateKeySet {
+            private_keys: private_keys
+        }
     }
 }
 
@@ -36,5 +59,19 @@ fn test_generate_private_key_set() {
     let bits = 2048;
     let priv_key = RSAPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
     
-    DistributedRSAPrivateKeySet::from_rsa_private_key(&priv_key)
+    let d = priv_key.d();
+    let primes = priv_key.primes();
+
+    let one = BigUint::from(1 as u16);
+    let lambda = (&primes[0].clone() - &one) * (&primes[1] - &one);
+
+    let keys = DistributedRSAPrivateKeySet::from_rsa_private_key(&priv_key, 10);
+
+    let mut sum = BigUint::from(0 as u16);
+    for key in keys.private_keys {
+        sum += key.d % lambda.clone();
+    }
+
+    let d = d % lambda;
+    assert_eq!(sum, d);
 }
